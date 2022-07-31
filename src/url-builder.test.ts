e99ce837b84a252b58aa1d06c063abb4c626a316
Goga Mamoulasvili <goga.mamoulasvili@gmail.com>
2022-07-31 15:26:39 +0200

    wip

--- src/url-builder.test.ts
+++ src/url-builder.test.ts
@@ -39,7 +39,7 @@ describe("URLBuilder", () => {
 		builder.setNethash(Networks["ark.mainnet"]);
 
 		expect(builder.generateTransfer("recipient", { memo: "memo" })).toBe(
-			"baseUrl?memo=memo&recipient=recipient&method=Transfer&coin=ARK&nethash=6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988",
+			"baseUrl?memo=memo&method=transfer&recipient=recipient&coin=ARK&nethash=6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988",
 		);
 	});
 
@@ -50,7 +50,7 @@ describe("URLBuilder", () => {
 		builder.setNethash(Networks["ark.mainnet"]);
 
 		expect(builder.generateTransfer("recipient", { amount: 1000 })).toBe(
-			"baseUrl?amount=1000&recipient=recipient&method=Transfer&coin=ARK&nethash=6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988",
+			"baseUrl?amount=1000&method=transfer&recipient=recipient&coin=ARK&nethash=6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988",
 		);
 	});
 
@@ -61,7 +61,7 @@ describe("URLBuilder", () => {
 		builder.setNethash(Networks["ark.mainnet"]);
 
 		expect(builder.generateTransfer("recipient")).toBe(
-			"baseUrl?method=Transfer&recipient=recipient&coin=ARK&nethash=6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988",
+			"baseUrl?method=transfer&recipient=recipient&coin=ARK&nethash=6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988",
 		);
 	});
 
