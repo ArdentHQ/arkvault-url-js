6855e6d60973bcf43515d48f2a3f7c8f7103c37a
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-05-13 18:24:04 -0600

    fix test

--- source/url-builder.test.ts
+++ source/url-builder.test.ts
@@ -154,7 +154,7 @@ describe("URLBuilder", ({ assert, it }) => {
 
 		assert.is(
 			builder.generateUsername("alfy"),
-			"baseUrl?method=username&nethash=6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988&username=alfy",
+			"baseUrl?method=username&username=alfy&coin=ARK&nethash=6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988",
 		);
 	});
 
