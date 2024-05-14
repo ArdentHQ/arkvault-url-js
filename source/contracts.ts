067f0d6c4487e243fa2fc9587562664a0b6af850
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-05-13 18:09:35 -0600

    remove type that is not needed

--- source/contracts.ts
+++ source/contracts.ts
@@ -39,16 +39,11 @@ type GenerateMessageVerifyOptions = {
 	method?: Methods.Verify;
 } & MessageVerifyOptions;
 
-type GenerateUsernameOptions = {
-	method?: Methods.Username;
-};
-
 export type {
 	BaseOptions,
 	GenerateMessageSignOptions,
 	GenerateMessageVerifyOptions,
 	GenerateTransferOptions,
-	GenerateUsernameOptions,
 	MessageSignOptions,
 	MessageVerifyOptions,
 	SignedMessage,
